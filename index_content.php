74484e822477734464e8630cc8deebccbfa2d564
meatbags <jxburrow@gmail.com>
2018-07-03 23:22:02 +1000

    2/n

--- index_content.php
+++ index_content.php
@@ -1,55 +1,17 @@
 
 <div class='sections'>
-  <div class='section'>
-    <div class='section__inner'>
-      Xavier Burrow is an animator and programmer working<br />
-      in three dimensions and five senses.<br /><br />
-      <a class='selectable' href='mailto:jxburrow@gmail.com'>jxburrow@gmail.com</a>
-    </div>
-  </div>
-
   <?php
     $query = new WP_Query(array('post_type' => 'projects', 'orderby' => 'menu_order', 'posts_per_page' => -1));
-    $i = 1;
-    $end = $query->post_count;
-
+    $i = 0;
     if ($query->have_posts()):
       while ($query->have_posts()):
-          $query->the_post();
-          $title = get_the_title();
-          $desc = get_field('description');
-          $gallery = get_field('gallery');
-          $links = get_field('links');
-          $classes = '';
-          if ($i == 1 || $i == 4 || $i == 7) {
-            $classes = 'open';
-          } else if ($i == 3 || $i == 6 || $i == $end) {
-            $classes = 'close margin-bottom';
-          }
-        ?>
-        <div class='section <?php echo $classes; ?>'>
-          <div class='section__inner'>
-            <div class='section-title'><?php echo $title; ?></div>
-            <div class='section-description'><?php echo $desc; ?></div>
-            <div class='section-links'>
-              <?php foreach($links as $link): ?>
-                <a class='link' href='<?php echo $link['url']; ?>' target='_blank'><?php echo $link['label']; ?></a>
-              <?php endforeach; ?>
-            </div>
-          </div>
-        </div>
-        <?php if ($i == 3 || $i == 6): ?>
-          <div class='space font-large'>&#9675;</div>
-        <?php if ($i == 3):
-            get_template_part('piano'); ?>
-            <div class='space font-large'>&#9675;</div>
-        <?php elseif ($i == 6):
-            get_template_part('chess'); ?>
-            <div class='space font-large'>&#9675;</div>
-        <?php
-          endif;
-        endif;
-        $i += 1;
+        $query->the_post();
+        get_template_part('index_content_project');
+        if (++$i == 3) {
+          get_template_part('piano');
+        } elseif ($i == 6) {
+          get_template_part('chess');
+        }
       endwhile;
     endif;
   ?>
