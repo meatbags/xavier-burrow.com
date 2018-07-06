4cf21a38c689d7911bbc714359f67891812b709b
meatbags <jxburrow@gmail.com>
2018-07-06 16:57:11 +1000

    window z ordering, chess window, buggs

--- chess.php
+++ chess.php
@@ -1,17 +1,8 @@
-<div class="chess">
-  <div class='left'>
-    <div class="title">
-      <div class="inner box no-border">
-        <br /><br />
-        Glucksberg v Najdorf<br />
-        Warsaw 1929, 0 - 1
-      </div>
-      <div class="reset box no-border">
-        reset board
-      </div>
+<div class='chess-wrapper display-none' id='chess-target'>
+  <div class='chess'>
+    <div class='chess-title'>
+      Glucksberg v Najdorf,<br />Warsaw 1929 (0 - 1)
     </div>
-  </div>
-  <div class="right">
     <div class='chess-board box no-border'>
       <ul>
         <li>♜</li><li></li><li></li><li></li><li></li><li>♜</li><li>♚</li><li></li>
@@ -24,17 +15,18 @@
         <li>♖</li><li></li><li>♗</li><li>♕</li><li></li><li>♖</li><li>♘</li><li></li>
       </ul>
     </div>
-  </div>
-  <div class="chess-reset-board">
-    <ul>
-      <li>♜</li><li>♞</li><li>♝</li><li>♛</li><li>♚</li><li>♝</li><li>♞</li><li>♜</li>
-      <li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li>
-      <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
-      <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
-      <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
-      <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
-      <li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li>
-      <li>♖</li><li>♘</li><li>♗</li><li>♕</li><li>♔</li><li>♗</li><li>♘</li><li>♖</li>
-    </ul>
+    <div class='chess-reset'>reset board</div>
+    <div class="chess-reset-board">
+      <ul>
+        <li>♜</li><li>♞</li><li>♝</li><li>♛</li><li>♚</li><li>♝</li><li>♞</li><li>♜</li>
+        <li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li><li>♟</li>
+        <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+        <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+        <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+        <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+        <li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li><li>♙</li>
+        <li>♖</li><li>♘</li><li>♗</li><li>♕</li><li>♔</li><li>♗</li><li>♘</li><li>♖</li>
+      </ul>
+    </div>
   </div>
 </div>
