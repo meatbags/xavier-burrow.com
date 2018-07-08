ecf5d472d7a208414f604e62f890b8be31763b7d
meatbags <jxburrow@gmail.com>
2018-07-09 00:38:00 +1000

    projects/ sections/ transparent formatting

--- index_content.php
+++ index_content.php
@@ -1,15 +1,18 @@
-<div class='sections'>
-  <?php
-    $query = new WP_Query(array('post_type' => 'projects', 'orderby' => 'menu_order', 'posts_per_page' => -1));
-    if ($query->have_posts()):
-      while ($query->have_posts()):
-        $query->the_post();
-        get_template_part('index_content_project');
-      endwhile;
-    endif;
-  ?>
+<div id='sections-target' class='sections display-none'>
+  <div class='sections__inner'>
+    <?php
+      $query = new WP_Query(array('post_type' => 'projects', 'orderby' => 'menu_order', 'posts_per_page' => -1));
+      if ($query->have_posts()):
+        while ($query->have_posts()):
+          $query->the_post();
+          get_template_part('index_content_project');
+        endwhile;
+      endif;
+    ?>
+  </div>
 </div>
 
 <?php get_template_part('piano'); ?>
 <?php get_template_part('chess'); ?>
 <?php get_template_part('business_card'); ?>
+<?php get_template_part('logo'); ?>
---
+++ logo.php
@@ -0,0 +1,5 @@
+<div id='logo-target' class='logo-wrapper display-none'>
+  <div class='logo'>
+    X.b
+  </div>
+</div>
