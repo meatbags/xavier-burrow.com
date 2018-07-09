380495473c8e43ffcdbc579bdf4f68e1da0a8014
meatbags <jxburrow@gmail.com>
2018-07-09 16:14:05 +1000

    images / galleries

--- index_content_project.php
+++ index_content_project.php
@@ -11,29 +11,39 @@
       <div class='section-title'><?php echo $title; ?></div>
     </div>
     <div class='section-body'>
-      <div class='section-description'>
-        <?php echo $desc; ?>
-      </div>
-      <?php if ($gallery): ?>
-        <div class='section-gallery'>
-          <div class='section-gallery__thumbs'>
-            <?php foreach($gallery as $img):
-              if (is_array($img) && array_key_exists('sizes', $img)):
-                $thumb = $img['sizes']['thumbnail'];
-                $src = $img['sizes']['large'];
-              ?>
-                <img src='<?php echo $thumb; ?>' data-src='<?php echo $thumb; ?>'>
-              <?php
-                endif;
-              endforeach;
-            ?>
+      <div class='section-body__inner'>
+        <div class='section-body__left'>
+          &#9758;
+        </div>
+        <div class='section-body__right'>
+          <div class='section-description'>
+            <?php echo $desc; ?>
+          </div>
+          <?php if ($gallery): ?>
+            <div class='section-gallery'>
+              <div class='section-gallery__thumbs'>
+                <?php foreach($gallery as $img):
+                  if (is_array($img) && array_key_exists('sizes', $img)):
+                    $thumb = $img['sizes']['thumbnail'];
+                    $src = $img['sizes']['large'];
+                  ?>
+                  <div class='gallery-image'>
+                    <img class='thumbnail' src='<?php echo $thumb; ?>'>
+                    <img class='full' src='<?php echo $src; ?>'>
+                  </div>
+                  <?php
+                    endif;
+                  endforeach;
+                ?>
+              </div>
+            </div>
+          <?php endif; ?>
+          <div class='section-links'>
+            <?php foreach($links as $link): ?>
+              <a class='link' href='<?php echo $link['url']; ?>' target='_blank'><?php echo $link['label']; ?></a>
+            <?php endforeach; ?>
           </div>
         </div>
-      <?php endif; ?>
-      <div class='section-links'>
-        <?php foreach($links as $link): ?>
-          <a class='link' href='<?php echo $link['url']; ?>' target='_blank'><?php echo $link['label']; ?></a>
-        <?php endforeach; ?>
       </div>
     </div>
   </div>
