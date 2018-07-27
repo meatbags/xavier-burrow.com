3c84b5d460c8f243b82ec3d0436502edaf3ee75c
meatbags <jxburrow@gmail.com>
2018-07-27 15:51:14 +1000

    value tween. noise animation

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php get_header(); ?>
 <div id='sections-current-image' class='sections-image'></div>
 
-<div class='wrapper'>
-  <div class='wrapper__inner'>
-    <?php get_template_part('index_content'); ?>
+<?php get_template_part('app/computer-ui'); ?>
+
+<div class='menu'>
+  <div id='menu-target' class='menu__inner'>
+    <div class='item'>
+      COMING SOON
+    </div>
   </div>
 </div>
 
-<?php get_template_part('computer-ui'); ?>
+<div class='wrapper'>
+  <div class='wrapper__inner'>
+    <div id='sections-target' class='sections display-none'>
+      <div class='sections__inner'>
+        <?php
+          $query = new WP_Query(array('post_type' => 'projects', 'orderby' => 'menu_order', 'posts_per_page' => -1));
+          if ($query->have_posts()):
+            while ($query->have_posts()):
+              $query->the_post();
+              get_template_part('index_project');
+            endwhile;
+          endif;
+        ?>
+      </div>
+    </div>
+
+    <?php get_template_part('app/piano'); ?>
+    <?php get_template_part('app/chess'); ?>
+    <?php get_template_part('app/business_card'); ?>
+    <?php get_template_part('app/logo'); ?>
+  </div>
+</div>
+
 <?php get_footer(); ?>
