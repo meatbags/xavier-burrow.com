ecab29ca32fb6d786364f768e722b0411154e807
meatbags <jxburrow@gmail.com>
2018-08-18 21:15:08 +1000

    conflicts res.

--- index.php
+++ index.php
@@ -22,29 +22,6 @@
   </div>
 </div>
 
-<<<<<<< HEAD
-=======
-<div class='contact-box'>
-  <div class='contact-box__header'>contact.</div>
-  <div class='contact-box__contact'>
-    <div class='email'>
-      <a href='mailto:jxburrow@gmail.com'>jxburrow@gmail.com</a> <a target='_blank' href='https://www.instagram.com/xavebabes'>insta</a>
-    </div>
-  </div>
-</div>
-<!-- <div class='more-box'>&darr;</div> -->
-
-<div class='canvas-wrapper'></div>
-
-<div class='background-image dimmed'>
-  <img src='<?php echo get_template_directory_uri(); ?>/lib/img/background-grey.jpg'/>
-</div>
-<div class='background-selected-image'>
-  <div class='pane active'></div>
-  <div class='pane'></div>
-</div>
-<div class='background-fill'></div>
->>>>>>> c5ef25f3b88fda9bffc8d17f98eada20c1a31fb4
 <div class='loading-screen'>
   <div class='loading-screen__inner'>
     loading.
