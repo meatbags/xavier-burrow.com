aa45400b5652418f671be592dce44bb4e599f8dc
meatbags <jxburrow@gmail.com>
2018-08-21 13:20:58 +1000

    production pack

--- header.php
+++ header.php
@@ -3,12 +3,12 @@
 <head>
 	<title><?php bloginfo('name'); ?></title>
 	<meta charset="utf-8" />
-	<meta name="description" content="Xavier Burrow is a web-designer, programmer, and animator.">
+	<meta name="description" content="Xavier Burrow is a web-developer and animator creating interactive spaces.">
   <meta name="keywords" content="Xavier Burrow, web design, interaction design, animation">
   <meta name="author" content="Xavier Burrow">
-	<meta property="og:url" content="" />
+	<meta property="og:url" content="http://xavierburrow.com/" />
 	<meta property="og:title" content="Xavier Burrow"/>
-	<meta property="og:image" content=""/>
+	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/lib/img/background-white.jpg"/>
 	<meta property="og:site_name" content="Xavier Burrow"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 	<link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
