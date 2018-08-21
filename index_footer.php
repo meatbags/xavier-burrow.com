81b159d7897de6bf49d73d863a05695cccd73b44
meatbags <jxburrow@gmail.com>
2018-08-21 16:41:12 +1000

    minor style update

--- index_footer.php
+++ index_footer.php
@@ -1,4 +1,5 @@
 <div class='footer'>
+  <div class='contact'>about me</div>
   <a href='mailto:jxburrow@gmail.com'>jxburrow@gmail.com</a>
   <a href='https://www.instagram.com/xavebabes/' target='_blank'>insta</a>
   <a class='link' href='https://github.com/meatbags' target='_blank'>github</a>
