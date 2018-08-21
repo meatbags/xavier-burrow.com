b5c971dc5da6ee02d1c49390acf2b000938a456e
meatbags <jxburrow@gmail.com>
2018-08-21 10:10:52 +1000

    draft 2 complete

--- index_footer.php
+++ index_footer.php
@@ -1,4 +1,5 @@
 <div class='footer'>
   <a href='mailto:jxburrow@gmail.com'>jxburrow@gmail.com</a>
   <a href='https://www.instagram.com/xavebabes/' target='_blank'>insta</a>
+  <a class='link' href='https://github.com/meatbags' target='_blank'>github</a>
 </div>
--- index_pane.php
+++ index_pane.php
@@ -2,10 +2,10 @@
   <div class='project-pane__inner'>
     <div class='back project-pane-close'><- back to projects</div>
     <div class='title'></div>
-    <!--<div class='line'></div>-->
+    <div class='line'></div>
     <div class='description'></div>
-    <div class='date'></div>
     <div class='video'></div>
+    <div class='date'></div>
     <div class='image'></div>
   </div>
 </div>
@@ -22,7 +22,7 @@
       </div>
     </div>
     <div class='description'>
-      Hi! I'm Xavier, a web designer and animator. I enjoy building interactive spaces.
+      Hi! I'm Xavier, a web developer and animator. I design and built interactive spaces. If you would like to work together, shoot me an email:
     </div>
     <div class='image'></div>
     <div class='video'></div>
